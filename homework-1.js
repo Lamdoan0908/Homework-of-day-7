f809159ca89e1d789ee2242ba750014eae507dbd
Lamdoan0908 <lamdoan310@gmail.com>
2025-09-20 17:24:26 +0700

    Add all project files

---
+++ homework-1.js
@@ -0,0 +1,7 @@
+let a=2;
+let b=3;
+console.log("Tổng:",a+b);
+console.log("Hiệu:",a-b);
+console.log("Nhân:",a*b);
+console.log("Dư:",a%b);
+console.log("a^b =",a**b)
---
+++ test-javascript.js
@@ -0,0 +1,12 @@
+let myName="Lan";
+let myAge=20;
+let isStudent=true;
+let EmptyValue=null;
+let unknownValue;
+
+
+console.log(typeof "hello");
+console.log(typeof 42);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
